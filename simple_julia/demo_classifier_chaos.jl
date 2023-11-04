0b1291d136d69dec778c2d4c2b9daebbfd344d08
Christopher Albert <albert@alumni.tugraz.at>
2023-11-04 16:33:39 +0100

    Chaos case

---
+++ simple_julia/demo_classifier_chaos.jl
@@ -0,0 +1,58 @@
+using Plots
+
+include("standard_map.jl")
+include("classifier.jl")
+
+tmax = 128
+K = 1.0
+
+q = Array{Float64}(undef, tmax+1)
+p = Array{Float64}(undef, tmax+1)
+
+q[1] = 0.5*pi
+p[1] = 1.2
+
+for k in 1:tmax
+    q[k+1], p[k+1] = standard_map(q[k], p[k], K)
+end
+
+z = [q';p']
+
+N1,too_short_time = count_single_recurrences(z)
+println("Recurrence numbers N1:")
+println(N1[.!too_short_time])
+
+
+# Plot the results
+plot(q, p, seriestype=:scatter, markersize=0.5, legend=false)
+xlims!(0, 2*pi)
+ylims!(0, 2*pi)
+xlabel!("q")
+ylabel!("p")
+savefig("chaos.png")
+
+tmax2 = 1024
+
+q2 = Array{Float64}(undef, tmax2+1)
+p2 = Array{Float64}(undef, tmax2+1)
+
+q2[1] = q[1]
+p2[1] = p[2]
+
+for k in 1:tmax2
+    q2[k+1], p2[k+1] = standard_map(q2[k], p2[k], K)
+end
+
+z = [q2';p2']
+
+N1,too_short_time = count_single_recurrences(z)
+println("Recurrence numbers N1:")
+println(N1[.!too_short_time])
+
+# Plot the results
+plot(q2, p2, seriestype=:scatter, markersize=0.5, legend=false)
+xlims!(0, 2*pi)
+ylims!(0, 2*pi)
+xlabel!("q")
+ylabel!("p")
+savefig("chaos2.png")
