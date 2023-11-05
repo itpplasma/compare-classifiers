0d9315a49b5739686d57e8982a574b0f620a3252
Christopher Albert <albert@alumni.tugraz.at>
2023-11-05 19:37:02 +0100

    Correct chaos case

--- simple_julia/demo_classifier_chaos.jl
+++ simple_julia/demo_classifier_chaos.jl
@@ -4,7 +4,7 @@ using Plots
 include("standard_map.jl")
 include("classifier.jl")
 
-tmax = 128
+tmax = 256
 K = 1.0
 
 q = Array{Float64}(undef, tmax+1)
@@ -20,7 +20,6 @@ end
 krec = find_recurrences(q)
 println("Classified as ideal: ", classify(krec))
 
-#%%
 # Plot the results
 plot(q, p, seriestype=:scatter, markersize=0.5, legend=false)
 xlims!(0, 2*pi)
@@ -29,20 +28,20 @@ xlabel!("q")
 ylabel!("p")
 savefig("chaos.png")
 
-tmax2 = 1024
+tmax2 = 512
 
 q2 = Array{Float64}(undef, tmax2+1)
 p2 = Array{Float64}(undef, tmax2+1)
 
 q2[1] = q[1]
-p2[1] = p[2]
+p2[1] = p[1]
 
 for k in 1:tmax2
     q2[k+1], p2[k+1] = standard_map(q2[k], p2[k], K)
 end
 
 krec2 = find_recurrences(q2)
-println("Classified as ideal: ", classify(krec))
+println("Classified as ideal: ", classify(krec2))
 
 # Plot the results
 plot(q2, p2, seriestype=:scatter, markersize=0.5, legend=false)
