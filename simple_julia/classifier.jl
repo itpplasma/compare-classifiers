e236a17910c909fabd8ec0e9e8d99e75743a5b27
Christopher Albert <albert@alumni.tugraz.at>
2023-11-05 10:22:44 +0100

    Better interval check

--- simple_julia/classifier.jl
+++ simple_julia/classifier.jl
@@ -11,13 +11,22 @@ function unwrap(x)
     return y
 end
 
+# https://math.stackexchange.com/questions/2275439/check-if-point-on-circle-is-between-two-other-points-on-circle
+function is_x3_between_x1_and_x2(x1, x2, x3)
+    x1_x2 = mod(x2 - x1 + 2π, 2π)
+    x1_x3 = mod(x3 - x1 + 2π, 2π)
+
+    return (x1_x2 <= π) ⊻ (x1_x3 > x1_x2)
+end
+
+
 function count_recurrences(z, order=1)
     nstep = size(z, 2)
 
     q = unwrap(z[1,:])
     p = z[2,:]
 
-    kpoi_interval = findall(qi -> q[1] <= mod(qi,2*pi) < q[2], q)
+    kpoi_interval = findall(qi -> is_x3_between_x1_and_x2(q[1], q[2], qi), q)
     npoi_interval = length(kpoi_interval)
 
     N = Array{Int32}(undef, npoi_interval)
@@ -27,7 +36,7 @@ function count_recurrences(z, order=1)
         too_short_time[i] = 1
         for k2 in k:nstep-1
             counter = 0
-            if mod(q[k2],2*pi) < q[1] && mod(q[k2+1],2*pi) >= q[1]
+            if is_x3_between_x1_and_x2(q[1], q[2], q[k2+1])
                 counter+=1
                 if counter == order
                     N[i] = k2-k
